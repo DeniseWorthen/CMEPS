733c6ff8f87f0778bdc401679a7d140302e68536
denise.worthen <Denise.Worthen@noaa.gov>
2021-01-18 11:08:24 -0700

    change alias for sa_ofrac; add to FBfrac(compatm)

--- mediator/med_fraction_mod.F90
+++ mediator/med_fraction_mod.F90
@@ -17,7 +17,7 @@ module med_fraction_mod
   !
   !  the fractions fields are defined for each grid in the fraction bundles as
   !    needed as follows.
-  !    character(*),parameter :: fraclist_a = 'ifrac:ofrac:lfrac
+  !    character(*),parameter :: fraclist_a = 'ifrac:ofrac:lfrac:onorm
   !    character(*),parameter :: fraclist_o = 'ifrac:ofrac:ifrad:ofrad'
   !    character(*),parameter :: fraclist_i = 'ifrac:ofrac'
   !    character(*),parameter :: fraclist_l = 'lfrac'
@@ -120,7 +120,7 @@ module med_fraction_mod
 
   integer, parameter                      :: nfracs = 5
   character(len=5)                        :: fraclist(nfracs,ncomps)
-  character(len=5),parameter,dimension(3) :: fraclist_a = (/'ifrac','ofrac','lfrac'/)
+  character(len=5),parameter,dimension(4) :: fraclist_a = (/'ifrac','ofrac','lfrac','onorm'/)
   character(len=5),parameter,dimension(4) :: fraclist_o = (/'ifrac','ofrac','ifrad','ofrad'/)
   character(len=5),parameter,dimension(2) :: fraclist_i = (/'ifrac','ofrac'/)
   character(len=5),parameter,dimension(1) :: fraclist_l = (/'lfrac'/)
@@ -594,8 +594,10 @@ contains
     real(r8), pointer          :: lfrac(:) => null()
     real(r8), pointer          :: ifrac(:) => null()
     real(r8), pointer          :: ofrac(:) => null()
+    real(r8), pointer          :: onorm(:) => null()
     real(r8), pointer          :: Si_ifrac(:) => null()
     real(r8), pointer          :: Si_imask(:) => null()
+    real(r8), pointer          :: Sa_ofrac(:) => null()
     type(ESMF_Field)           :: lfield
     type(ESMF_Field)           :: field_src
     type(ESMF_Field)           :: field_dst
@@ -708,6 +710,17 @@ contains
              call med_map_field(field_src, field_dst, is_local%wrap%RH(compice,compatm,:), maptype, rc=rc)
              if (ChkErr(rc,__LINE__,u_FILE_u)) return
           end if
+
+          ! Set 'onorm' from FBImp(compatm) to FBfrac(compatm)
+          !if (trim(coupling_mode) == 'nems_frac') then
+          if (trim(coupling_mode) == 'nems_orig' .or. trim(coupling_mode) == 'nems_frac') then
+             call fldbun_getdata1d(is_local%wrap%FBImp(compatm,compatm), 'Sa_ofrac', Sa_ofrac, rc)
+             if (ChkErr(rc,__LINE__,u_FILE_u)) return
+             call fldbun_getdata1d(is_local%wrap%FBFrac(compatm), 'onorm', onorm, rc)
+             if (ChkErr(rc,__LINE__,u_FILE_u)) return
+             onorm(:) = Sa_ofrac(:)
+          end if
+
        end if ! end of if present compatm
        call t_stopf('MED:'//trim(subname)//' fbfrac(compatm)')
 
