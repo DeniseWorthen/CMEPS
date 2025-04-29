68297fdbc42b178fafac1b5eb30e69a81317e210
denise.worthen <Denise.Worthen@noaa.gov>
2025-04-29 15:52:52 +0000

    fix typo

--- mediator/esmFldsExchange_ufs_mod.F90
+++ mediator/esmFldsExchange_ufs_mod.F90
@@ -32,7 +32,7 @@ contains
     use med_internalstate_mod , only : compmed, compatm, compocn, compice, complnd, compwav, ncomps
     use med_internalstate_mod , only : mapbilnr, mapconsf, mapconsd, mappatch, mappatch_uv3d
     use med_internalstate_mod , only : mapfcopy, mapnstod, mapnstod_consd, mapnstod_consf
-    use med_internalstate_mod , only : mapconsf_aofrac, mapbilnr_nstodd
+    use med_internalstate_mod , only : mapconsf_aofrac, mapbilnr_nstod
     use med_internalstate_mod , only : coupling_mode, mapnames, samegrid_atmlnd
     use esmFlds               , only : med_fldList_type
     use esmFlds               , only : addfld_to => med_fldList_addfld_to
